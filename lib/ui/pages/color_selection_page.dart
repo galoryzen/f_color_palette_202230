589777d3bba1d261d17acfaaf7cc3db70769372a
Raul Lopez <rjlopezgr@gmail.com>
2022-08-14 17:09:52 -0500

    Finished

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -11,7 +11,7 @@ class ColorSelectionPage extends StatelessWidget {
   void showDialog() {
     Get.dialog(const AlertDialog(
       title: Text('Flutter'),
-      content: Text('Dialog'),
+      content: Text('Que viva el Barca'),
     ));
   }
 
@@ -19,15 +19,55 @@ class ColorSelectionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Color palette app'),
+          title: const Text('Color palette app 2-6'),
           actions: [
-            IconButton(onPressed: showDialog, icon: Icon(Icons.info_outline))
+            IconButton(onPressed: showDialog, icon: const Icon(Icons.info_outline))
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
+        body: (Align(
+          alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ColorPalette(
+                  baseColor: '100720',
+                  baseColor1: '31087B',
+                  baseColor2: 'FA2FB5', 
+                  baseColor3: 'FFC23C', 
+                  callback: showColor),
+                ColorPalette(
+                  baseColor: '411530',
+                  baseColor1: 'D1512D',
+                  baseColor2: 'F5C7A9', 
+                  baseColor3: 'F5E8E4', 
+                  callback: showColor),
+                ColorPalette(
+                  baseColor: '293462',
+                  baseColor1: 'D61C4E',
+                  baseColor2: 'FEB139', 
+                  baseColor3: 'FFF80A', 
+                  callback: showColor),
+                ColorPalette(
+                  baseColor: '231955',
+                  baseColor1: '1F4690',
+                  baseColor2: 'E8AA42', 
+                  baseColor3: 'FFE5B4', 
+                  callback: showColor),
+                ColorPalette(
+                  baseColor: '3330E4',
+                  baseColor1: 'F637EC',
+                  baseColor2: 'FBB454', 
+                  baseColor3: 'FAEA48', 
+                  callback: showColor),
+                ColorPalette(
+                  baseColor: '000000',
+                  baseColor1: '1A4D2E',
+                  baseColor2: 'FF9F29', 
+                  baseColor3: 'FAF3E3', 
+                  callback: showColor),
+              ],
+            )),
         )));
   }
 
@@ -36,7 +76,7 @@ class ColorSelectionPage extends StatelessWidget {
     Clipboard.setData(ClipboardData(text: value));
     Get.snackbar(
       'Color palette',
-      'Valor copiado',
+      'Valor copiado, que viva el Barca',
       backgroundColor: ColorUtils.FromHex(value),
       snackPosition: SnackPosition.BOTTOM,
     );
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -5,15 +5,23 @@ class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
     // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.baseColor,
+    required this.baseColor1,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.callback
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +29,49 @@ class ColorPalette extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
-    );
+      child: Row(
+        children: [
+          Expanded(
+            flex: 10,
+            child: GestureDetector(
+              onTap: () => callback(baseColor),
+              child: Container(
+                height: 100,
+                color: ColorUtils.FromHex(baseColor),
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 6,
+            child: GestureDetector(
+              onTap: () => callback(baseColor1),
+              child: Container(
+                height: 100,
+                color: ColorUtils.FromHex(baseColor1),
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 4,
+            child: GestureDetector(
+              onTap: () => callback(baseColor2),
+              child: Container(
+                height: 100,
+                color: ColorUtils.FromHex(baseColor2),
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () => callback(baseColor3),
+              child: Container(
+                height: 100,
+                color: ColorUtils.FromHex(baseColor3),
+                ),
+              ),
+          ),
+        ]
+    ));
   }
 }
